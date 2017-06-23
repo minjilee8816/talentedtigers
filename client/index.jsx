72632fa66bf1cd544c6dd18a88aac3a8f7b1431d
Eric Mai <enmai1988@gmail.com>
2017-06-22 18:39:55 -0700

    created ticketSubmission component

--- client/index.jsx
+++ client/index.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import TicketList from './components/ticketList.jsx';
-// import TicketSubmission from './components/ticketSubmission.jsx';
+import TicketSubmission from './components/ticketSubmission.jsx';
 // import Nav from './components/nav.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      ticketList: [{name: 'Test'}, {name: 'Test2'}, {name: 'Test3'}]
+      ticketList: [{name: 'Test'}, {name: 'Test2'}, {name: 'Test3'}],
+      ticketOptionList: ['React', 'Socket.IO', 'Recursion', 'Postgres']
     };
+  }
+
+  handleTicketSubmission(e) {
+    e.preventDefault();
+    let ticket = {
+      description: document.getElementById('ticket_submission_description').value,
+      category: document.getElementById('ticket_option_dropdown').value,
+      status: 'Opened'
+    };
+    console.log(`Sending Descrip: ${ticket.description}, Category: ${ticket.category} to api/tickets via POST`);
+    $.ajax({
+      url: 'api/tickets',
+      type: 'POST',
+      data: ticket,
+      success: (response) => {
+        console.log(`Successfully sent ${ticket} to apt/tickets via POST`);
+      },
+      error: () => {
+        console.log('Error submitting ticket to api/tickets via POST');
+      }
+    });
   }
 
   render() {
     return (
       <div className="col-md-8">
-        {/* <Nav />
-        <TicketSubmission /> */}
+        {/* <Nav />*/}
+        <TicketSubmission handleTicketSubmission={this.handleTicketSubmission.bind(this)} ticketOptionList={this.state.ticketOptionList}/>
         <TicketList ticketList={this.state.ticketList}/>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const db = require ('../database/');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/'));
 
+app.post('/api/tickets', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+});
 
 app.listen(3000, () => console.log('listening on port 3000'));
