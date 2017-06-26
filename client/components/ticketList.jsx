ffce24384998337eb6c5bf88866a6e678942cb07
David Vassett <david@davidvassett.com.au>
2017-06-26 15:33:04 -0700

    add ticketListEntry

--- client/components/ticketList.jsx
+++ client/components/ticketList.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import TicketEntry from './ticketEntry.jsx';
 
 const TicketList = (props) => (
-  <ul className="ticket_list">
+  <div className="ticket_list">
     {props.ticketList.map((ticket, index) => <TicketEntry ticket={ticket} key={index}/>)}
-  </ul>
+  </div>
 );
 
 export default TicketList;
