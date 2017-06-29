2293d25bbc0e0be0b0e88943cc6e6de4b65bc011
Eric Mai <enmai1988@gmail.com>
2017-06-29 13:42:19 -0700

    able to update all tickets for all users

--- client/index.jsx
+++ client/index.jsx
@@ -48,8 +48,11 @@ class App extends React.Component {
   }
 
   getTickets(option) {
-    $.get('/api/tickets', option, (tickets) => {
-      this.setState({ ticketList: tickets });
+    $.get('/api/tickets', () => {
+      this.socket.emit('get tickets', option);
+      this.socket.on('reply', data => {
+        this.setState({ ticketList: data.ticketList });
+      });
     });
   }
 
