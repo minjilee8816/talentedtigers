51dccafc1ccefb66bbbdc55dd71fc59830ddd42f
Eric Mai <enmai1988@gmail.com>
2017-06-29 15:45:12 -0700

    package

--- database/migrate.js
+++ database/migrate.js
@@ -1,7 +1,7 @@
 const { db, Ticket, User } = require('./index');
 const sampleData = require('./sampleData');
 
-db.sync({ force: true })
+db.sync()
   .then(() => {
     User.bulkCreate(sampleData.users);
     Ticket.bulkCreate(sampleData.tickets);
