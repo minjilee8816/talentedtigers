e433d0b9611176e9dd1dcf7c7cf781ab1ff7b05c
David Vassett <david@davidvassett.com.au>
2017-06-30 16:41:24 -0700

    add validation to add user form

--- client/components/addUser.jsx
+++ client/components/addUser.jsx
@@ -2,27 +2,41 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const AddUser = () => {
+
   const addNewUser = (e) => {
-    e.preventDefault();
-    let user = {
-      firstName: document.getElementById('first-name').value,
-      lastName: document.getElementById('last-name').value,
-      username: document.getElementById('user-name').value,
-      role: document.getElementById('role-dropdown').value
-      // cohort: document.getElementById('cohort-name').value,
-    };
-    console.log(`Sending new user ${user.username} to api/users via POST`);
-    $.ajax({
-      url: 'api/users',
-      type: 'POST',
-      data: user,
-      success: (response) => {
-        console.log(`Successfully sent ${user} to api/users via POST`);
+    $('.add_user_form').validate({
+      rules: {
+        firstName: 'required',
+        lastName: 'required',
+        username: 'required',
+        cohortName: 'required',
+        role: 'required'
       },
-      error: () => {
-        console.log('Error submitting ticket to api/users via POST');
-      }
-    });
+      submitHandler: (form) => {
+        let user = {
+          firstName: document.getElementById('first-name').value,
+          lastName: document.getElementById('last-name').value,
+          username: document.getElementById('user-name').value,
+          role: document.getElementById('role-dropdown').value,
+          cohort: document.getElementById('cohort-name').value
+        };
+        $.ajax({
+          url: 'api/users',
+          type: 'POST',
+          data: user,
+          success: (response) => {
+            document.getElementById('first-name').value = '';
+            document.getElementById('last-name').value = '';
+            document.getElementById('user-name').value = '';
+            document.getElementById('cohort-name').value = '';
+          },
+          error: () => {
+            console.log('Error submitting ticket to api/users via POST');
+          }
+        });
+      },
+      errorPlacement: function(error, element) {} // Do not show error messages
+    });   
   };
 
   return (
@@ -33,25 +47,25 @@ const AddUser = () => {
       <div className="form-group row">
         <div className="col-xs-4">
           <label htmlFor="first-name">First Name</label>
-          <input className="form-control" id="first-name" placeholder="First Name" />
+          <input className="form-control" id="first-name" name="firstName" placeholder="First Name" />
         </div>
         <div className="col-xs-4">
           <label htmlFor="last-name">Last Name</label>
-          <input className="form-control" id="last-name" placeholder="Last Name" />
+          <input className="form-control" id="last-name" name="lastName" placeholder="Last Name" />
         </div>
         <div className="col-xs-4">
           <label htmlFor="user-name">Username</label>
-          <input className="form-control" id="user-name" placeholder="Username" />
+          <input className="form-control" id="user-name" name="username" placeholder="Username" />
         </div>
       </div>
       <div className="row">
         <div className="col-xs-4">
           <label htmlFor="cohort-name">Cohort</label>
-          <input className="form-control" id="cohort-name" placeholder="Cohort Name" />
+          <input className="form-control" id="cohort-name" name="cohortName" placeholder="Cohort Name" />
         </div>
         <div className="col-xs-4">
           <label htmlFor="role-dropdown">Role</label>
-          <select className="form-control" id="role-dropdown">
+          <select className="form-control" id="role-dropdown" name="role">
             <option value="admin">Admin</option>
             <option value="mentor">Mentor</option>
             <option value="student">Student</option>
