e78208d85e42bbbd211e871d29882cbe600b0f80
Jacob Penney <jacobpenney@Jacobs-MacBook-Pro.local>
2017-06-30 15:15:22 -0700

    predict wait time

--- helpers/util.js
+++ helpers/util.js
@@ -1,28 +1,44 @@
 
-const computeAvgWaitTime = (tickets) => {
-  let sum = tickets.reduce((a, b) => {
-    return a + Math.abs((new Date(b.claimedAt) - new Date(b.createdAt)));
+const reducedToDay = date => date % 604800000 < 86400000;
+
+const computeAvgWaitTime = (tickets, mentors, userId) => {
+  const storage = [];
+  let length = tickets.length;
+  let count = 0;
+  let sum = tickets.reduce((acc, curr) => {
+    let date = Date.parse(curr.claimedAt);
+    let wait = date - Date.parse(curr.createdAt);
+    if(reducedToDay(date) && curr.claimedAt) { 
+      storage.push(curr);
+      return acc + wait; 
+    } 
+    return acc;
   }, 0);
-  return sum / tickets.length;
-};
-
-const computeAveTicketOpeningTime = (tickets) => {
-  if (tickets.length <= 1) { return 0; }
-  let diff = [];
-  for (let i = 0; i < tickets.length - 1; i++) {
-    diff.push(Date.parse(tickets[i + 1].createdAt) - Date.parse(tickets[i].createdAt));
+  let queuePos = tickets.filter((ticket) => { return ticket.status === 'Opened' }).sort((ticket1, ticket2) => Date.parse(ticket1.createdAt) - Date.parse(ticket2.createdAt)).findIndex((ticket) => ticket.userId == userId ) + 1;
+  console.log('queuePos: ', queuePos);
+  console.log('list of open tickets: ', tickets.filter((ticket) => { return ticket.status === 'Opened' }).sort((ticket1, ticket2) => Date.parse(ticket1.createdAt) - Date.parse(ticket2.createdAt)));
+  let quantityClaimedAndUnclosed = tickets.filter((ticket) => { return ticket.claimedAt && !ticket.closedAt }).length;
+  let openTickets = tickets.filter((ticket) => { return ticket.status == 'Opened' });
+  // keep this line for realtime data and delete line 21 with the hard code:
+  // let excessMentors = mentors - quantityClaimedAndUnclosed;
+  const excessMentors = 2;
+  const estimatedInterval = new Date(sum / storage.length).getUTCMinutes();
+  let estimate = 0;
+  let countAvail = excessMentors;
+  if (queuePos >= 0) {
+    for(let i = 0; i < queuePos; i++) {
+      if(i+1 < countAvail && countAvail) {
+        console.log('in the first conditional', estimatedInterval / (excessMentors*excessMentors));
+        estimate += estimatedInterval / (excessMentors*excessMentors);
+      } else {
+        estimate += estimatedInterval / excessMentors/*optimized with all mentors on duty rather than excess*/;
+      }
+    }
   }
-  return diff.reduce((a, b) => { return a + b; }) / diff.length;
+  
+  return Math.floor(estimate);
 };
-
-const computeCurrWaitTime = (totalAveWait, gap, index) => {
-  if (!gap) { return 0; }
-  return (totalAveWait - gap) * index;
-};
-
 
 module.exports = {
-  computeAvgWaitTime: computeAvgWaitTime,
-  computeAveTicketOpeningTime: computeAveTicketOpeningTime,
-  computeCurrWaitTime: computeCurrWaitTime
+  computeAvgWaitTime: computeAvgWaitTime
 };
