e90eb8c19ac89f4a5c3dffa2ebbd7075a4925d9e
David Vassett <david@davidvassett.com.au>
2017-07-01 14:52:32 -0700

    add avatar to the navbar

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -7,7 +7,7 @@ const Nav = ({statistic, user}) => (
       <a className="navbar-brand" href="#">Help Reactor</a>
       <div className="dropdown navbar-right">
         <div className="dropdown-toggle navbar-profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          <img src="http://placehold.it/40x40" />
+          <img src={user.avatarUrl} />
         </div>
         <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
           <li><a href="/api/logout">Logout</a></li>
