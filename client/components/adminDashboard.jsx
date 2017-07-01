bd1894d44d2cd18bf8fdb36bbcb6cbf940d464d0
Eric Mai <enmai1988@gmail.com>
2017-07-01 00:13:37 -0700

    fixed server crash

--- client/components/adminDashboard.jsx
+++ client/components/adminDashboard.jsx
@@ -6,14 +6,14 @@ import AdminFilter from './adminFilter.jsx';
 const AdminDashboard = ({filterTickets, statistic, ticketCategoryList}) => (
   <div id="admin_dashboard">
     <div className="col-xs-12 admin_dashboard_title"><h2>Admin Dashboard</h2></div>
-    <h4>Active Students:  {statistic.studentNum}
-      <span className='admin_dashboard_span'>Current Open Tickets:  {statistic.open}</span>
+    <h4>Active Students: {statistic.studentNum}
+      <span className='admin_dashboard_span'>Current Open Tickets: {statistic.open}</span>
     </h4>
-    <h4>Active Mentors:  {statistic.mentorNum}
-      <span className='admin_dashboard_span'>Tickets Closed Today:  {statistic.closed}</span>
+    <h4>Active Mentors: {statistic.mentorNum}
+      <span className='admin_dashboard_span'>Tickets Closed Today: {statistic.closed}</span>
     </h4>
-    <h4>Current Average Wait Time:  {statistic.currAveWait} minutes
-      <span className='admin_dashboard_span'>Most Popular Category Today:  {statistic.popularCategory}</span>
+    <h4>Current Average Wait Time: {statistic.currAveWait} minutes
+      <span className='admin_dashboard_span'>Most Popular Category Today: {statistic.popularCategory}</span>
     </h4>
     <AddUser />
     <AdminFilter filterTickets={filterTickets} ticketCategoryList={ticketCategoryList}/>
