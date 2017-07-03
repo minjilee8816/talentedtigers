a59c733a8db02963f34e08549535be14285f3ac9
Jacob Penney <jacobpenney@Jacobs-MacBook-Pro.local>
2017-07-03 12:12:53 -0700

    commit

--- models/index.js
+++ models/index.js
@@ -1,18 +1,25 @@
-const { db, Ticket, User } = require('../database/');
-const _ = require('underscore');
-const util = require('../helpers/util');
+'use strict';
 
+var _require = require('../database/'),
+    db = _require.db,
+    Ticket = _require.Ticket,
+    User = _require.User;
 
-const createTicket = (req, res) => {
-  Ticket.create(req.body).then(result => {
-    if (!result) { res.sendStatus(500); }
+var _ = require('underscore');
+var util = require('../helpers/util');
+
+var createTicket = function createTicket(req, res) {
+  Ticket.create(req.body).then(function (result) {
+    if (!result) {
+      res.sendStatus(500);
+    }
     res.sendStatus(201);
   });
 };
 
-const findTickets = (req, res) => {
-  let option = {};
-  let query = req.query;
+var findTickets = function findTickets(req, res) {
+  var option = {};
+  var query = req.query;
   if (query.role === 'student') {
     option = { userId: query.id };
   } else if (query.role === 'mentor') {
@@ -24,30 +31,24 @@ const findTickets = (req, res) => {
     option = _.omit(query, ['id', 'role']);
   }
 
-  const otherfunction = function(status) {
+  var otherfunction = function otherfunction(status) {
     console.log('test', status);
     return status;
   };
 
   Ticket.findAll({
     where: option,
-    include: [ { model: User, as: 'user' }, { model: User, as: 'userClaimed' } ],
-    order: [
-      [db.literal(`CASE
-        WHEN status = 'Claimed' THEN 1
-        WHEN status = 'Opened' THEN 2
-        WHEN status = 'Closed' THEN 3
-        END`
-      )],
-      ['updatedAt', 'DESC']
-    ]
-  }).then(result => {
-    if (!result) { res.sendStatus(404); }
+    include: [{ model: User, as: 'user' }, { model: User, as: 'userClaimed' }],
+    order: [[db.literal('CASE\n        WHEN status = \'Claimed\' THEN 1\n        WHEN status = \'Opened\' THEN 2\n        WHEN status = \'Closed\' THEN 3\n        END')], ['updatedAt', 'DESC']]
+  }).then(function (result) {
+    if (!result) {
+      res.sendStatus(404);
+    }
     res.send(result);
   });
 };
 
-const updateTickets = (req, res) => {
+var updateTickets = function updateTickets(req, res) {
   console.log('trying to update tickets: ', req.body);
   if (req.body.status === 'Claimed') {
     req.body.claimedAt = new Date();
@@ -55,21 +56,20 @@ const updateTickets = (req, res) => {
   if (req.body.status === 'Closed') {
     req.body.closedAt = new Date();
   }
-  Ticket.update(req.body, { where: { id: req.params.id } })
-    .then(result => {
-      if (!result) { res.sendStatus(500); }
-      res.sendStatus(200);
-    });
+  Ticket.update(req.body, { where: { id: req.params.id } }).then(function (result) {
+    if (!result) {
+      res.sendStatus(500);
+    }
+    res.sendStatus(200);
+  });
 };
 
-const createUser = (req, res) => {
-  User.findOrCreate({ where: req.body })
-    .then((user) => {
-      res.send(true);
-    })
-    .catch(err => {
-      res.send(false);
-    });
+var createUser = function createUser(req, res) {
+  User.findOrCreate({ where: req.body }).then(function (user) {
+    res.send(true);
+  }).catch(function (err) {
+    res.send(false);
+  });
 };
 
 module.exports = {
