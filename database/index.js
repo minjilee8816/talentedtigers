6f73580cde9b2af5c19c65cbb7be24314657e83e
Tyler Pate <tylerrosspate@gmail.com>
2017-07-05 15:59:12 -0700

    Update User schema to include Rating (#2)

--- database/index.js
+++ database/index.js
@@ -36,7 +36,8 @@ const User = db.define('user', {
   lastName: Sequelize.STRING,
   username: { type: Sequelize.STRING, allowNull: false, unique: true },
   role: { type: Sequelize.ENUM('student', 'mentor', 'admin'), allowNull: false },
-  cohort: Sequelize.STRING
+  cohort: Sequelize.STRING,
+  rating: Sequelize.INTEGER
 });
 
 const Feedback = db.define('feedback', {
