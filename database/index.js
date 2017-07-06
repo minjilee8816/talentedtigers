8ad64adf3afb205b6f8ac3ecda700c1317aabd05
Tyler Pate <tylerrosspate@gmail.com>
2017-07-05 17:28:47 -0700

    Update User Schema (#5)

--- database/index.js
+++ database/index.js
@@ -37,7 +37,7 @@ const User = db.define('user', {
   username: { type: Sequelize.STRING, allowNull: false, unique: true },
   role: { type: Sequelize.ENUM('student', 'mentor', 'admin'), allowNull: false },
   cohort: Sequelize.STRING,
-  rating: Sequelize.INTEGER
+  rating: {type: Sequelize.INTEGER, allowNull: true}
 });
 
 const Feedback = db.define('feedback', {
